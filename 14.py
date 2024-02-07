9792ddc1c6bae62cfa835ecb79154c0f4e59177f
Andrei <andrej_ilin7@mail.ru>
2024-02-07 19:50:59 +0300

    task2

---
+++ 14.py
@@ -0,0 +1,23 @@
+"""
+Напишите функцию, которая найдет самую длинную строку с общим префиксом среди массива строк.
+Если общего префикса нет, верните пустую строку"".
+"""
+
+strs = ["flower", "flow", "flight"]
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        res = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                res += i[0]
+            else:
+                return res
+        return res
+
+
+
+
+sol = Solution()
+print(sol.longestCommonPrefix(strs))
---
+++ 181.py
@@ -0,0 +1,19 @@
+"""
+"""
+import pandas as pd
+
+data = [[1, 'Joe', 70000, 3], [2, 'Henry', 80000, 4], [3, 'Sam', 60000, None], [4, 'Max', 90000, None]]
+employee = pd.DataFrame(data, columns=['id', 'name', 'salary', 'managerId']).astype(
+    {'id': 'Int64', 'name': 'object', 'salary': 'Int64', 'managerId': 'Int64'})
+
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+
+    # emp = employee.loc[employee["managerId"].isin(employee["id"].tolist())]
+    emp = employee.merge(employee, how='inner',  left_on='managerId', right_on='id')
+    emp_earn = emp[emp.salary_x > emp.salary_y].name_x
+
+    return pd.DataFrame({"Employee" : emp_earn})  # employee["id"] == employee["managerId"],
+
+
+print(find_employees(employee))
