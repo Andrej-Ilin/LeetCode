f848fb46266741b757e3f5675a3bb4d2a653c5d2
Andrei <andrej_ilin7@mail.ru>
2024-05-12 22:14:39 +0300

    67

--- 67.py
+++ 67.py
@@ -12,7 +12,8 @@ Output: "10101"
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        summ = bin(int(a, 2) + int(b, 2))
-        return summ[2:]
+        # print(int(a, 2))
+        summ =format(int(a, 2) + int(b, 2), 'b')
+        return summ
 sol = Solution()
 print(sol.addBinary("11", "1")) #"100"
